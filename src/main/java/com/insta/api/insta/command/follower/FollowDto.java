08a311c650c06ec77d6973afdf70747f46aa9033
alicjakropa <alakrop@gmail.com>
2022-08-14 14:34:05 +0100

    removed CommentLikeDto

--- src/main/java/com/insta/api/insta/command/follower/FollowDto.java
+++ src/main/java/com/insta/api/insta/command/follower/FollowDto.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 public class FollowDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long followerUserId;
+
     @NotNull(message = "Id of user to follow can't be empty")
     private Long toFollowUserId;
 }
