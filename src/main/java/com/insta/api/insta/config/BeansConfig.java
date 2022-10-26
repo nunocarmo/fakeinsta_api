06228882cd2821288acde2911b5a6169060e7535
Nuno <nuno.carmo@mindswap.academy>
2022-10-26 13:34:08 +0100

    confi cors

--- src/main/java/com/insta/api/insta/config/BeansConfig.java
+++ src/main/java/com/insta/api/insta/config/BeansConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.TimeUnit;
 
@@ -36,6 +38,14 @@ public class BeansConfig {
         caffeineCacheManager.setCaffeine(caffeine);
         return caffeineCacheManager;
     }
-
+    @Bean
+    public WebMvcConfigurer coresConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 
 }
